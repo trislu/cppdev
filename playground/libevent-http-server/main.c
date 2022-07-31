f94276ff70d2d81efdc9cbab502d54ed4883a29c
Lu Kai <lukai@mail.ustc.edu.cn>
2022-07-31 21:15:03 +0800

    [cleanup] remove playground demos

--- playground/libevent-http-server/main.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include "demo.h"
-
-int main(int argc, char **argv)
-{
-    demo_run(argc, argv);
-    printf("done\n");
-    return 0;
-}
--- playground/libevent-http-server/src/demo.h
+++
@@ -1,6 +0,0 @@
-#ifndef _DEMO_H
-#define _DEMO_H
-
-int demo_run(int argc, char **argv);
-
-#endif
